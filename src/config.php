d9b38282c3effb4b91d0b2cda0f6ba4145c551d7
CCaonguyen <caonguyen2262003@gmail.com>
2023-04-10 17:42:45 +0700

    abc

---
+++ src/config.php
@@ -0,0 +1,14 @@
+<?php 
+$currency 2 = '&#8377; '; //Currency Character or code 3 
+$db_username = 'dbcao';
+$db_password = 'dbCao@123';
+$db_name = 'ecommerceweb'; 
+ $db_host = 'db';
+ $shipping_cost = 1.50; //shipping cost 
+ $taxes = array( 'VAT' => 12,
+ 				'Service Tax'=>5);
+ $mysqli = new mysqli($db_host, $db_username, $db_password, $db_name);
+ if ($mysqli->connect_error) {
+ 	die('Error: ('. $mysqli->connect_errno.') '. $mysqli->connect_error); 
+ }
+  ?>
